54440da86478ddf38013af976da3a84201499287
ninjaa <alkhudrisaid83@gmail.com>
2020-09-26 13:45:17 +0700

    first commit

---
+++ 2.js
@@ -0,0 +1,6 @@
+nilaiSantri = [50, 90, 65, 67, 89, 80, 50, 97, 84];
+var hasil = 0
+for (i = 0; i < nilaiSantri.length; i++) {
+    hasil += nilaiSantri[i];
+}
+document.write(hasil / nilaiSantri.length)
